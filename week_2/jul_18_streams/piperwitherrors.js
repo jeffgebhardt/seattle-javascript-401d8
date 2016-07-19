4e1de5a4012bbe96622c0fbd75674a6c2c9a269b
Rebecca Peltz <rebeccapeltz@gmail.com>
2016-07-18 19:16:56 -0700

    streams code

---
+++ week_2/jul_18_streams/piperwitherrors.js
@@ -0,0 +1,18 @@
+'use strict';
+const fs = require('fs');
+var piper = module.exports = exports = () => {
+  var readFileName = process.argv.length > 2 ? process.argv[2] : __dirname + '/public/index1.html';
+  var writeFileName = process.argv.length > 3 ? process.argv[3] : __dirname + '/public/copy.html';
+  var reader = fs.createReadStream(readFileName);
+
+  reader.on('open', () => {
+    var writer = fs.createWriteStream(__dirname + writeFileName);
+    reader.pipe(writer);
+    reader.pipe(process.stdout);
+  });
+  reader.on('error', (err) => {
+    console.log('error', err);
+  });
+
+};
+piper();
